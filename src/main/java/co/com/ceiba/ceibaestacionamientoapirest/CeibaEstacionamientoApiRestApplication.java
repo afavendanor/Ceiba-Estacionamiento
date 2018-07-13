738953ff58e13d603ec978e6797ac43d851b0678
CEIBA\andres.avendano <andres.avendano@ceiba.com.co>
2018-07-13 11:00:20 -0500

    Se configura proyecto inicial

---
+++ src/main/java/co/com/ceiba/ceibaestacionamientoapirest/CeibaEstacionamientoApiRestApplication.java
@@ -0,0 +1,12 @@
+package co.com.ceiba.ceibaestacionamientoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaEstacionamientoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaEstacionamientoApiRestApplication.class, args);
+	}
+}
